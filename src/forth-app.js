fdef440432e0ac9af3a66123af0a863251e30f1b
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2023-12-25 23:42:22 -0800

    2023 refresh

--- src/forth-app.js
+++ src/forth-app.js
@@ -1,75 +1,73 @@
-(function () {
-    'use strict';
+'use strict';
 
-    var forth = require('forth');
+const forth = require('forth');
 
-    var inp, oup, cmForth, cmJavaSCript, comments, f, myTimeOut;
+let comments = true;
+const inp = document.getElementById('inp');
+const oup = document.getElementById('oup');
 
-    function refresh () {
-        var time = Date.now();
-        var source = cmForth.getValue();
-        var consolebuffer = '';
-        var refreshTimeout;
-        f = forth();
-        f.s.write(source);
-        f.s.on('data', function (chunk) {
-            consolebuffer += chunk;
-            clearTimeout(refreshTimeout);
-            refreshTimeout = setTimeout(function () {
-                cmJavaSCript.setValue(consolebuffer);
-            }, 100);
-        });
-        console.log((Date.now() - time) + 'ms');
-        localStorage.forthSourceCode = source;
-    }
+inp.value = localStorage.forthSourceCode || ': foo if 1 else 2 ;\nsee foo\n';
+oup.value = '?';
 
-    comments = true;
-    inp = document.getElementById('inp');
-    oup = document.getElementById('oup');
+const cmForth = CodeMirror.fromTextArea(
+  inp,
+  {
+    mode: 'forth',
+    theme: 'colorforth',
+    lineNumbers: true,
+    lineWrapping: true,
+    indentUnit: 2,
+    tabSize: 2,
+    autofocus: false
+  }
+);
 
-    inp.value = localStorage.forthSourceCode || ': foo 1 2 + ;';
-    oup.value = '?';
+const cmJavaSCript = CodeMirror.fromTextArea(
+  oup,
+  {
+    mode: 'javascript',
+    lineNumbers: true,
+    indentUnit: 2,
+    tabSize: 2,
+    matchBrackets: true,
+    autoCloseBrackets: true,
+    highlightSelectionMatche: true,
+    autofocus: true
+  }
+);
 
-    document.getElementById('button').addEventListener('click', function () {
-        comments = !comments;
-        refresh();
-    });
+let myTimeOut;
+cmForth.on('change', function () {
+  clearTimeout(myTimeOut);
+  myTimeOut = setTimeout(refresh, 750);
+});
 
-    cmForth = CodeMirror.fromTextArea(
-        inp,
-        {
-            mode: 'forth',
-            theme: 'colorforth',
-            lineNumbers: true,
-            lineWrapping: true,
-            indentUnit: 2,
-            tabSize: 2,
-            autofocus: false
-        }
-    );
+function refresh () {
+  const time = Date.now();
+  const source = cmForth.getValue();
+  let consolebuffer = '';
+  let refreshTimeout;
+  const f = forth();
+  f.s.write(source);
+  f.s.on('data', function (chunk) {
+    consolebuffer += chunk;
+    clearTimeout(refreshTimeout);
+    refreshTimeout = setTimeout(function () {
+      cmJavaSCript.setValue(consolebuffer);
+    }, 100);
+  });
+  console.log((Date.now() - time) + 'ms');
+  localStorage.forthSourceCode = source;
+}
 
-    cmJavaSCript = CodeMirror.fromTextArea(
-        oup,
-        {
-            mode: 'javascript',
-            lineNumbers: true,
-            indentUnit: 2,
-            tabSize: 2,
-            matchBrackets: true,
-            autoCloseBrackets: true,
-            highlightSelectionMatche: true,
-            autofocus: true
-        }
-    );
+document.getElementById('button').addEventListener('click', function () {
+  comments = !comments;
+  refresh();
+});
 
+global.FORTH = {
+  refresh
+};
 
-    cmForth.on('change', function () {
-        clearTimeout(myTimeOut);
-        myTimeOut = setTimeout(refresh, 750);
-    });
-
-    refresh();
-})();
-
-/* global console, CodeMirror */
-/* env browser */
+/* global CodeMirror */
+/* eslint-env browser */
